f89a4f8cb9a13c87debd485eecc732c2888c0150
jus1234 <osjamg1996@gmail.com>
2024-04-16 19:30:33 +0900

    config: gitignore 세팅

---
+++ CalendarApp/CalendarApp/CalendarAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  CalendarAppApp.swift
+//  CalendarApp
+//
+//  Created by Wooseok on 4/16/24.
+//
+
+import SwiftUI
+
+@main
+struct CalendarAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
